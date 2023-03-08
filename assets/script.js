fde1d43c08f05eb4680982592227b31b182788aa
Gabriel Lourenço <glourenco150@gmail.com>
2023-03-08 15:07:43 -0300

    Atualização seção sobre

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,23 @@
+
+let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+
+/*animação*/
 window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
@@ -6,3 +26,18 @@ window.addEventListener('load', () => {
       mirror: false
     })
   });
+
+
+  /*back-to-top*/
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
